4e397536b0c314e71c1a28c8f8407ca345b71e57
Adarsh P <adarsh@Adarshs-MacBook-Pro.local>
2025-12-06 23:04:26 +0530

    updated, simplified and modified function names and code

--- 2019/Day_5/src/asteroids.js
+++ 2019/Day_5/src/asteroids.js
@@ -2,19 +2,23 @@ const add = (x, y) => x + y;
 
 const mul = (x, y) => x * y;
 
-const addOperation = (index, int1, code, int2) => {
+const lessThan = (int1, int2) => int1 < int2 ? 1 : 0;
+
+const equals = (int1, int2) => int1 === int2 ? 1 : 0;
+
+const utils = (index, int1, int2, code, operation) => {
   const outputIndex = +code[index + 3];
-  code[outputIndex] = add(int1, int2);
+  code[outputIndex] = operation(int1, int2);
   return index + 4;
 }
 
-const mulOperation = (index, int1, code, int2) => {
-  const outputIndex = +code[index + 3];
-  code[outputIndex] = mul(int1, int2);
-  return index + 4;
-}
+const addOperation = (index, int1, int2, code) =>
+  utils(index, int1, int2, code, add);
 
-const addInput = (index, int1, code) => {
+const mulOperation = (index, int1, int2, code) =>
+  utils(index, int1, int2, code, mul);
+
+const addInput = (index, int1, int2, code) => {
   code[+code[index + 1]] = prompt('enter num');
   return index + 2;
 };
@@ -24,27 +28,21 @@ const print = (index, int1) => {
   return index + 2;
 }
 
-const jumpIfTrue = (index, int1, code, int2) =>
+const jumpIfTrue = (index, int1, int2) =>
   int1 !== 0 ? int2 : index + 3;
 
-const jumpIfFalse = (index, int1, code, int2) =>
+const jumpIfFalse = (index, int1, int2) =>
   int1 === 0 ? int2 : index + 3;
 
-const lessThan = (index, int1, code, int2) => {
-  const outputIndex = +code[index + 3];
-  code[outputIndex] = int1 < int2 ? 1 : 0;
-  return index + 4;
-}
+const lessThanOperation = (index, int1, int2, code) =>
+  utils(index, int1, int2, code, lessThan);
 
-const equals = (index, int1, code, int2) => {
-  const outputIndex = +code[index + 3];
-  code[outputIndex] = int1 === int2 ? 1 : 0;
-  return index + 4;
-}
+const equalsOperation = (index, int1, int2, code) =>
+  utils(index, int1, int2, code, equals);
 
 const opcodes = {
   1: addOperation, 2: mulOperation, 3: addInput, 4: print, 5: jumpIfTrue,
-  6: jumpIfFalse, 7: lessThan, 8: equals, 99: "halt"
+  6: jumpIfFalse, 7: lessThanOperation, 8: equalsOperation, 99: "halt"
 };
 
 const immediateValue = (code, i, addOn) => +code[i + addOn];
@@ -75,28 +73,28 @@ export const findMode = (code, index) => {
   return modeType[key](code, index);
 }
 
-export const codeProcess = (code, opCode, index) => {
+export const executeInstruction = (code, opCode, index) => {
   const [int1, int2] = findMode(code, index);
   if (opCode === 'halt') return;
-  return opCode(index, int1, code, int2);
+  return opCode(index, int1, int2, code);
 };
 
-export const process = (code, i) => {
+export const evaluateOperation = (code, i) => {
   const op = opcodes[+code[i] % 100];
 
   if (op !== 'halt') {
-    i = codeProcess(code, op, i);
+    i = executeInstruction(code, op, i);
   }
 
   return [op, i];
 };
 
-export const processCode = (intCode) => {
+export const evaluateIntCode = (intCode) => {
   const code = intCode.split(",");
   let { index, opCode } = { index: 0, opCode: '' };
 
   while (opCode !== "halt") {
-    const [op, i] = process(code, index);
+    const [op, i] = evaluateOperation(code, index);
     index = i;
     opCode = op;
   }
--- 2019/Day_5/test/asteroids_test.js
+++ 2019/Day_5/test/asteroids_test.js
@@ -3,19 +3,19 @@ import * as allfns from "../src/asteroids.js";
 
 // Deno.test('main test 1', () => {
 //   const input = Deno.readTextFileSync("Data/input.txt");
-//   assertEquals(allfns.processCode(input),9775037)
+//   assertEquals(allfns.evaluateIntCode(input),9775037)
 // });
 
-Deno.test("input : 1,0,0,0,99" , () =>
-  assertEquals(allfns.processCode("1,0,0,0,99"), "2,0,0,0,99"));
+Deno.test("input : 1,0,0,0,99", () =>
+  assertEquals(allfns.evaluateIntCode("1,0,0,0,99"), "2,0,0,0,99"));
 
 Deno.test("input : 2,4,4,5,99,0", () =>
-  assertEquals(allfns.processCode("2,4,4,5,99,0"), "2,4,4,5,99,9801"));
+  assertEquals(allfns.evaluateIntCode("2,4,4,5,99,0"), "2,4,4,5,99,9801"));
 
 Deno.test("input : 1101,100,-1,4,0", () =>
-  assertEquals(allfns.processCode("1101,100,-1,4,0"), "1101,100,-1,4,99"));
+  assertEquals(allfns.evaluateIntCode("1101,100,-1,4,0"), "1101,100,-1,4,99"));
 
 // Deno.test('main test 2', () => {
 //   const input = Deno.readTextFileSync("Data/input.txt");
-//   assertEquals(allfns.processCode(input),15586959)
+//   assertEquals(allfns.evaluateIntCode(input),15586959)
 // });
